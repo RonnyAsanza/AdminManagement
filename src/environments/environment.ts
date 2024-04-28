096f67a8b9a3311e7c9d70753d53b85633c9cd62
masanzaz <thsrr10@gmail.com>
2024-04-28 21:59:59 +0200

    Add new environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
     //PROD
-    //apiPermitsURL: 'https://permitapi.preciserd.com/Permits/PermitApi/api/v1/',
-    //apiTranslationServiceURL: 'https://permitapi.preciserd.com/Permits/TranslationService/api/v1/',
+    //apiPermitsURL: 'https://permit-service.preciseparklink.com/PermitApi/api/v1/',
+    //apiTranslationServiceURL: 'https://permit-service.preciseparklink.com/TranslationsAPI/api/v1/',
     //permitsURL: 'https://permit-service.preciseparklink.com/images/',
 
     //DEV
-    apiPermitsURL: 'https://permit-service.preciseparklink.com/PermitApi/api/v1/',
-    apiTranslationServiceURL: 'https://permit-service.preciseparklink.com/TranslationsAPI/api/v1/',
+    apiPermitsURL: 'https://permitapi.preciserd.com/Permits/PermitApi/api/v1/',
+    apiTranslationServiceURL: 'https://permitapi.preciserd.com/Permits/TranslationService/api/v1/',
     permitsURL: 'https://permitapi.preciserd.com/images/',
 
     //Local
